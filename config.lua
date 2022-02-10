8c655a4cccf761f9e73fa224e6ce99d6105e106f
NugiTv <nugitv@gmail.com>
2022-02-10 09:23:21 -0600

    some small change to code

--- config.lua
+++ config.lua
@@ -152,3 +152,10 @@ Config.CarWash = { -- carwash
         ["coords"] = vector3(-699.62, -932.7, 19.01),
     }
 }
+
+--//minigame
+maxDice = 6						-- Maximum number of dice to roll			Default = 6
+maxDiceSides = 100				-- Maximum number of sides on dice			Default = 100
+rpsPrefix = "Showed: "			-- Rock, Paper, Scissors result prefix		Default = "Showed: "
+flipPrefix = "Flipped: "		-- Flip Coin result prefix					Default = "Flipped: "
+rollPrefix = "Rolled: "			-- Roll Dice result prefix					Default = "Rolled: "
